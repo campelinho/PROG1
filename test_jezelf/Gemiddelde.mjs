b27aabaf95220b336f9eff68bd537267fbdec6b5
Gabriel <campelinho_1957@outlook.be>
2024-09-26 10:02:46 +0200

    oefening

--- test_jezelf/Gemiddelde.mjs
+++ test_jezelf/Gemiddelde.mjs
@@ -2,5 +2,25 @@ import * as readline from 'node:readline/promises';
 import{stdin as input, stdout as output} from 'node:process';
 const userInput = readline.createInterface({input, output});
 
+let gemiddelde = 0;
 
 let jeGetal1 = parseFloat(await userInput.question('Geef een getal in:'));
+ gemiddelde += jeGetal1;
+
+ jeGetal1 = parseFloat(await userInput.question('Geef een getal in:'));
+ gemiddelde += jeGetal1;
+
+
+ jeGetal1 = parseFloat(await userInput.question('Geef een getal in:'));
+ gemiddelde += jeGetal1;
+ 
+
+ jeGetal1 = parseFloat(await userInput.question('Geef een getal in:'));
+ gemiddelde += jeGetal1;
+
+ gemiddelde = gemiddelde / 4;
+
+ console.log("de gemiddelde is " + gemiddelde);
+
+ process.exit();
+ 
