2412c3c8fa3f4f8d88e266d4b7755ff57320bda4
Gabriel <campelinho_1957@outlook.be>
2024-09-26 17:12:02 +0200

    Rekenmachine

---
+++ Module_2/Rekenmachine.mjs
@@ -0,0 +1,52 @@
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+import { clear } from 'node:console';
+const userInput = readline.createInterface({input, output});
+
+console.log("Welke berekeningen wil je uitvoeren?");
+console.log("1.Optelling");
+console.log("2.Aftrekking");
+console.log("3.Product")
+console.log("4.Deling");
+
+
+let rekenen = await userInput.question('keuze:');
+
+let Getal1 = parseFloat(await userInput.question('Geef een eerste getal in:'));
+
+let Getal2 = parseFloat(await userInput.question('Geef een tweede getal in:'));
+
+
+
+let resultaat = 0;
+
+switch(rekenen)
+{
+    case "1":
+       resultaat = Getal1 + Getal2; 
+       console.log(resultaat);
+    break;
+
+    case "2":
+           resultaat = Getal1 - Getal2; 
+           console.log(resultaat);
+    break;
+
+    case "3":
+           resultaat = Getal1 * Getal2;
+           console.log(resultaat);
+        break;
+
+     case "4":
+           resultaat = Getal1 / Getal2;
+           console.log(resultaat);
+         break;
+
+       default:
+       console.log("Berekening niet mogelijk");
+
+
+}
+
+
+process.exit();
