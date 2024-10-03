c43332338dbf619d84442803ac308c8ccd533ee8
Gabriel <campelinho_1957@outlook.be>
2024-10-03 11:41:00 +0200

    oef

---
+++ Module_3/M3_Priemgetal.mjs
@@ -0,0 +1,26 @@
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+import { clear } from 'node:console';
+const userInput = readline.createInterface({input, output});
+ 
+ 
+let getal = parseFloat(await userInput.question('Geef een getal:'));
+ 
+ 
+    for(let i = 2; i<= getal/2;i++)
+    {
+        if(getal%i==0)
+        {
+            console.log("Het getal " + getal + " is geen priemgetal");
+        }
+        else
+        {
+            console.log("Het getal " + getal + " is een priemgetal");
+        }
+        break;
+    }
+    
+    
+ 
+ 
+process.exit();
--- Module_3/Sterrenpiramide.mjs
+++ Module_3/Sterrenpiramide.mjs
@@ -7,18 +7,7 @@ let hoogte = await userInput.question("kies een hoogte");
 
 for (let i = 1; i <= hoogte; i++)
     {
-        
-    let rij = "*"
+    let rij = "*".repeat(i);
     console.log(rij);
-
-if (i<=hoogte)
-    {
-        i++
-        console.log(rij);
     }
-
-    }
-    
-
-
 process.exit();
