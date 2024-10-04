8f8749cba833b93fc61a542d0e4373d2c70ded79
Gabriel <campelinho_1957@outlook.be>
2024-10-04 09:39:21 +0200

    oef

--- Module_3/M4_FizzBuzz.mjs
+++ Module_3/M4_FizzBuzz.mjs
@@ -9,8 +9,12 @@ const userInput = readline.createInterface({input, output});
 for(let i=1; i<=100;i++)
 {
 
+if(i % 3 == 0 && i % 5 == 0)
+{
+console.log(i +" FizzBuzz");
+}
 
-if (i % 3 == 0)
+else if (i % 3 == 0)
 {
 console.log(i+" Fizz");
 }
@@ -18,10 +22,6 @@ console.log(i+" Fizz");
 else if(i % 5 == 0)
 {
 console.log(i+" Buzz");
-}
-else if(i % 3 == 0 && i % 5 == 0)
-{
-console.log(i +" FizzBuzz");
 }
 
 }
