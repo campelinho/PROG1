5243a45a21d8d3e079deb99c160838824f922ac9
Gabriel <campelinho_1957@outlook.be>
2024-10-04 11:24:02 +0200

    Update M4_LopendeGemiddelde.mjs

--- Module_3/M4_LopendeGemiddelde.mjs
+++ Module_3/M4_LopendeGemiddelde.mjs
@@ -2,22 +2,25 @@ import * as readline from 'node:readline/promises';
 import{stdin as input, stdout as output} from 'node:process';
 import { clear } from 'node:console';
 const userInput = readline.createInterface({input, output});
-
-let getal;
-let gemiddelde =0;
-let teller=0;
-
-do
-{
-    getal = parseFloat(await userInput.question('Geef een getal:'));
-    gemiddelde+=getal;
+ 
+ 
+let gemiddelde = 0;
+let teller = 0;
+let som = 0;
+ 
+ 
+do {
+ 
+    let getal = parseFloat(await userInput.question('Geef een getal:'));
+    som += getal;
     teller++;
-    gemiddelde = gemiddelde/teller;
-    
+ 
+    gemiddelde = som / teller;
+ 
     console.log(gemiddelde);
-}
-
-while(gemiddelde<=25)
-
-
-process.exit();
+ 
+   
+   
+} while (gemiddelde <= 25);
+ 
+process.exit()
