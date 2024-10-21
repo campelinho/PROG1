45ecc11706f63a9a4cd456eac5cd3ff73a26bead
Gabriel <campelinho_1957@outlook.be>
2024-10-21 11:58:53 +0200

    Create M6_Sort.mjs

---
+++ Module_6/M6_Sort.mjs
@@ -0,0 +1,11 @@
+
+
+let getallen =[-2,6,-8,10,-74,5,20,115,-56,65]
+
+console.log("Originele array");
+console.log(getallen);
+
+getallen.sort(function (a, b) { return b - a });
+
+console.log("nummers gerangschikked")
+console.log(getallen);
